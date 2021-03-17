fc5f0b91432ee721fbd21d84105181c910173737
AKpython <72851630+akpythonyt@users.noreply.github.com>
2021-03-17 08:14:17 +0530

    a voice assistant with Wikipedia integration

--- virassitant.py
+++ virassitant.py
@@ -1,25 +1,25 @@
-import speech_recognition as sr
-import yagmail
+import speech_recognition as sr 
+import wikipedia
+import pyttsx3
 
+engine=pyttsx3.init()
 recognizer=sr.Recognizer()
 with sr.Microphone() as source:
-    print('Clearing background noise..')
+    print('Clearing background noise...Please wait')
     recognizer.adjust_for_ambient_noise(source,duration=1)
     print("waiting for your message...")
     recordedaudio=recognizer.listen(source)
-    print('Done recording..!')
-try:
-    print('Printing the message..')
-    text=recognizer.recognize_google(recordedaudio,language='en-US')
+    print('Done recording')
 
-    print('Your message:{}'.format(text))
+try:
+    print('Printing your message...Please wait')
+    text=recognizer.recognize_google(recordedaudio,language='en-US')
+    print('Your Message:{}',format(text))
 
 except Exception as ex:
     print(ex)
 
-#Automate mails:
-
-reciever='queriesakpython@yahoo.com'
-message=text
-sender=yagmail.SMTP('atest0684@gmail.com')
-sender.send(to=reciever,subject='This is an automated mail',contents=message)
+#Input data
+wikisearch=wikipedia.summary(text)
+engine.say(wikisearch)
+engine.runAndWait()
